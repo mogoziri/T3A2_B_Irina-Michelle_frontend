19ff177711c5e36257abe5c07a2dec1daed88af3
Michelle Ohannessian <michelleohannessian@Michelles-MBP.modem>
2023-03-09 11:31:27 +1100

    added car items,updated car Listing and car details pages

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
 
+import carData from "../data/CarData";
+import CarItem from '../components/UI/CarItem';
+
+import { Container, Row, Col } from "reactstrap";
+
+/* Needs to connect to Book Now Button on Car Listing Page*/ 
 const CarDetails = () => {
-    return <div>CarDetails</div>;
+    return (
+    <Helmet title="Car Details">
+
+        {/* car offer section  */}
+        <section>
+            <Container>
+                <Row>
+                    <Col lg='12'className="text-center mb-5">
+                        <h6 className="section__subtitle">Car Details</h6>
+                        <h2 className="section__title">Car Info</h2>
+                    </Col>
+
+                    {
+                        carData.map(item => (
+                            <CarItem item={item} key={item.id} />
+                        ))
+                    }
+                </Row>
+            </Container>
+        </section>
+
+        </Helmet>
+    );
 };
 
 export default CarDetails
