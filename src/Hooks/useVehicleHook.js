0132ef1fb7ec4c69371d1082e64637d5877bcff8
Michelle Ohannessian <michelleohannessian@Michelles-MBP.modem>
2023-03-14 00:03:30 +1100

    amended Hooks file

--- src/Hooks/useVehicleHook.js
+++ src/Hooks/useVehicleHook.js
@@ -3,12 +3,12 @@ import { useNavigate, /*useLocation*/ } from "react-router-dom";
 import * as vehiclesApi from "../API/api-vehicles"
 
 const useVehicle = () => {
-    const [/*listVehicle,*/ setVehicle] = useState();
-    const [/*user,*/ setUser] = useState();
-    const [/*error,*/ setError] = useState();
-    const [/*isLoggedIn,*/ setIsLoggedIn] = useState(false);
-    const [/*loading,*/ setLoading] = useState(false);
-    const [/*loadingInitial,*/ setLoadingInitial] = useState(true);
+    // const [/*listVehicle,*/ setVehicle] = useState();
+    // const [/*user,*/ setUser] = useState();
+    // const [/*error,*/ setError] = useState();
+    // const [/*isLoggedIn,*/ setIsLoggedIn] = useState(false);
+    // const [/*loading,*/ setLoading] = useState(false);
+    // const [/*loadingInitial,*/ setLoadingInitial] = useState(true);
     console.log(useVehicle)
     const navigate = useNavigate() 
 
@@ -17,15 +17,13 @@ const useVehicle = () => {
   
       vehiclesApi
         .listVehicle({ type, owner_id, price_per_day, location, availability, features, picture_url })
-        .then((vehicle) => {
-          console.log({vehicle});
+        .then(() => {
           navigate('/');
         })
-        .catch((error) => {
-          // console.log({error})
-          setError(error.response.data.data);    
+        .catch(() => {
+          // setError(error.response.data.data);    
         })
-        .finally(() => setLoading(false));
+        // .finally(() => setLoading(false));
     };
 
     return {
