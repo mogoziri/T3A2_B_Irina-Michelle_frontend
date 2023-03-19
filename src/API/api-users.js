b75b9ced28804449f184b8bf0887663e6ab1a5bf
Michelle Ohannessian <michelleohannessian@Michelles-MBP.modem>
2023-03-19 11:21:08 +1100

    comments on API files

--- src/API/api-users.js
+++ src/API/api-users.js
@@ -1,42 +1,46 @@
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 
+//set up URL env for development and production
 const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? process.env.REACT_APP_DEV_BASE_URL
     : process.env.REACT_APP_PROD_BASE_URL;
 
+//get current user with token
 export const getCurrentUser = async () => {
   const token = localStorage.getItem("user-token");
-
+// set up authorization with a bearer token
   if (token) {
     const response = await axios.get(`${BASE_URL}/users/profile`, {
       headers: { Authorization: `Bearer ${token}` },
     });
     const { _id } = response.data;
-
+//if the user is in the DB return userID 
     return { isValid: true, userId: _id };
   }
-
+//otherwise return false (error message is sent)
   return { isValid: false };
 };
 
+//signup with a username and password. 
 export const signUp = async ({ username, password }) => {
   const response = await axios.post(`${BASE_URL}/users/register`, {
     username,
     password,
   });
-  const { id } = jwt_decode(response.data);
+  const { id } = jwt_decode(response.data); //decode cookie to receive the id associated with the user.
 
-  return { id, token: response.data };
+  return { id, token: response.data };//user has now signed up and can now access bookings and add cars to database.
 };
 
+//login with a username and password. 
 export const logIn = async ({ username, password }) => {
   const response = await axios.post(`${BASE_URL}/users/login`, {
     username,
     password,
   });
-  const { id } = jwt_decode(response.data);
+  const { id } = jwt_decode(response.data); //decode cookie to receive the id associated with the user.
 
-  return { id, token: response.data };
+  return { id, token: response.data }; //user is logged in and can now access bookings and add cars to database.
 };
--- src/API/api-vehicles.js
+++ src/API/api-vehicles.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+//set up URL env for development and production
 const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? process.env.REACT_APP_DEV_BASE_URL
     : process.env.REACT_APP_PROD_BASE_URL;
 
+//list a Vehicle 
 export const listVehicle = async ({
   description,
   transmission,
@@ -14,11 +16,12 @@ export const listVehicle = async ({
   availability,
   picture_url,
 }) => {
-  const token = localStorage.getItem("user-token");
+  const token = localStorage.getItem("user-token"); //get Item with user-token
   const response = await axios.post(
     `${BASE_URL}/vehicles`,
     {
-      /*from server.js file BE*/ description,
+      /*from server.js file BE*/ 
+      description,
       transmission,
       owner_id,
       price_per_day,
@@ -26,12 +29,13 @@ export const listVehicle = async ({
       availability,
       picture_url,
     },
-    { headers: { Authorization: `Bearer ${token}` } }
+    { headers: { Authorization: `Bearer ${token}` } } //authorization required with a bearer token
   );
 
   return response.data;
 };
 
+//update a Vehicle 
 export const updateVehicle = async ({
   description,
   transmission,
@@ -42,11 +46,12 @@ export const updateVehicle = async ({
   availability,
   picture_url,
 }) => {
-  const token = localStorage.getItem("user-token");
+  const token = localStorage.getItem("user-token"); //get Item with user-token
   const response = await axios.put(
     `${BASE_URL}/vehicles/${vehicle_id}`,
     {
-      /*from server.js file BE*/ description,
+      /*from server.js file BE*/ 
+      description,
       transmission,
       owner_id,
       price_per_day,
@@ -54,30 +59,34 @@ export const updateVehicle = async ({
       availability,
       picture_url,
     },
-    { headers: { Authorization: `Bearer ${token}` } }
+    { headers: { Authorization: `Bearer ${token}` } } //user can update vehicle with authorization using bearer code
   );
 
   return response.data;
 };
 
+//Retrieve a Vehicle using vehicle ID
 export const getVehicle = async (id) => {
   const response = await axios.get(`${BASE_URL}/vehicles/${id}`);
 
   return response.data;
 };
 
+//Retrieve all Vehicles
 export const getAllVehicles = async () => {
   const response = await axios.get(`${BASE_URL}/vehicles`);
 
   return response.data;
 };
 
+//A user can retrieve all Vehicles that belong to them 'the owner' with the owner_id
 export const getVehiclesForOwner = async (ownerId) => {
   const response = await axios.get(`${BASE_URL}/users/${ownerId}/vehicles`);
 
   return response.data;
 };
 
+//create a reservation on a vehicle with vehicle_id and user_token 
 export const createReservation = async (vehicle_id, renter_id) => {
   const token = localStorage.getItem("user-token");
   const response = await axios.post(
@@ -86,12 +95,13 @@ export const createReservation = async (vehicle_id, renter_id) => {
       reserveFrom: new Date().toJSON(),
       reserveTo: new Date().toJSON(),
     },
-    { headers: { Authorization: `Bearer ${token}` } }
+    { headers: { Authorization: `Bearer ${token}` } } //authorization required to reserve a vehicle
   );
 
   return response.data;
 };
 
+//Owner can accept or decline reservation by a user
 export const updateReservationStatus = async (reservation_id, status) => {
   const token = localStorage.getItem("user-token");
   const response = await axios.put(
@@ -99,27 +109,29 @@ export const updateReservationStatus = async (reservation_id, status) => {
     {
       status: status,
     },
-    { headers: { Authorization: `Bearer ${token}` } }
+    { headers: { Authorization: `Bearer ${token}` } } //authorization required 
   );
 
   return response.data;
 };
 
+//Retrieve owners reservations 
 export const getOwnerReservations = async (ownerId) => {
   const token = localStorage.getItem("user-token");
   const response = await axios.get(
     `${BASE_URL}/users/owner/${ownerId}/reservations`,
-    { headers: { Authorization: `Bearer ${token}` } }
+    { headers: { Authorization: `Bearer ${token}` } } //authorization required 
   );
 
   return response.data;
 };
 
+//Retrieve renters reservations 
 export const getRenterReservations = async (renterId) => {
   const token = localStorage.getItem("user-token");
   const response = await axios.get(
     `${BASE_URL}/users/${renterId}/reservations`,
-    { headers: { Authorization: `Bearer ${token}` } }
+    { headers: { Authorization: `Bearer ${token}` } }//authorization required 
   );
 
   return response.data;
